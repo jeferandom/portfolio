185bce3da5c2c8d114095a2c5055d6dd5334b045
jef <contactjefjs@gmail.com>
2026-08-22 11:12:12 -0500

    tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jef.github.io',
   base: '/portfolio',
   output: 'static',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
